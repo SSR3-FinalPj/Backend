3e0c60ab5cfc5107a81bb62e5d2a9edf1693f52d
SJM-coding <greatspirit637@gmail.com>
2025-08-06 13:36:27 +0900

    Initial commit

---
+++ src/main/java/org/example/ssj3pj/Ssj3PjApplication.java
@@ -0,0 +1,13 @@
+package org.example.ssj3pj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ssj3PjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ssj3PjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/ssj3pj/Ssj3PjApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ssj3pj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ssj3PjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
