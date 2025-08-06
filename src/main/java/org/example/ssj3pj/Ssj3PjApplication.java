b538da4c2eb8c0ea850219ffac0718bf30485ea1
SJM-coding <greatspirit637@gmail.com>
2025-08-06 13:39:50 +0900

    setting

--- src/main/java/org/example/ssj3pj/Ssj3PjApplication.java
+++ src/main/java/org/example/ssj3pj/Ssj3PjApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Ssj3PjApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Ssj3PjApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Ssj3PjApplication.class, args);
+    }
 
 }
