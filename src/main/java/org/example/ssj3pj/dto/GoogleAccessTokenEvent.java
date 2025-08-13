e76cd8c2d89bf4feba2e60fda7ee28a213862767
SJM-coding <greatspirit637@gmail.com>
2025-08-13 16:43:29 +0900

    :sparkle: 구글토큰 kafka 설정

---
+++ src/main/java/org/example/ssj3pj/dto/GoogleAccessTokenEvent.java
@@ -0,0 +1,18 @@
+package org.example.ssj3pj.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+//kafka로 토큰 보낼 dto
+@Data
+@NoArgsConstructor   // 기본 생성자
+@AllArgsConstructor  // 모든 필드를 받는 생성자
+public class GoogleAccessTokenEvent {
+    private Long userId;
+    private String provider;
+    private String accessToken;
+    private long expiresAtEpochSec;
+    private String event;
+}
