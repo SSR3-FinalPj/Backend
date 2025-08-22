ee1d653446389d114aad51962e430ff88ca5cc34
SJM-coding <greatspirit637@gmail.com>
2025-08-22 14:30:46 +0900

    :sparkle: S3설정

---
+++ src/main/java/org/example/ssj3pj/services/StorageService.java
@@ -0,0 +1,61 @@
+package org.example.ssj3pj.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
+
+import java.time.Duration;
+
+@Service
+@RequiredArgsConstructor
+public class StorageService {
+
+    private final S3Client s3;
+    private final S3Presigner presigner;
+
+    @Value("${aws.s3.bucket}")
+    private String bucket;
+
+    @Value("${aws.presign.ttl-minutes:15}")
+    private long ttlMinutes;
+
+    /** 비디오 키 규칙 */
+    public String videoKey(String videoId) {
+        return "videos/" + videoId + "/master.mp4";
+    }
+
+    /** 업로드용 Presigned PUT URL */
+    public String presignPut(String key, String contentType) {
+        var put = PutObjectRequest.builder()
+                .bucket(bucket)
+                .key(key)
+                .contentType(contentType)
+                .build();
+        var pre = PutObjectPresignRequest.builder()
+                .putObjectRequest(put)
+                .signatureDuration(Duration.ofMinutes(ttlMinutes))
+                .build();
+        return presigner.presignPutObject(pre).url().toString();
+    }
+
+    /** 다운로드/재생용 Presigned GET URL */
+    public String presignGet(String key, String responseContentType) {
+        var get = GetObjectRequest.builder()
+                .bucket(bucket)
+                .key(key)
+                .responseContentType(responseContentType)
+                .responseContentDisposition("inline")
+                .build();
+        var pre = GetObjectPresignRequest.builder()
+                .getObjectRequest(get)
+                .signatureDuration(Duration.ofMinutes(ttlMinutes))
+                .build();
+        return presigner.presignGetObject(pre).url().toString();
+    }
+}
